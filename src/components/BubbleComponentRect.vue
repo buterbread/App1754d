a6dd2af41f7cd5e96df5acb6e76e5f119a22f3b0
fara <buterbread@gmail.com>
2020-03-25 18:40:49 +0200

    Alpha 0.1 WIP

--- src/components/BubbleComponentRect.vue
+++ src/components/BubbleComponentRect.vue
@@ -3,11 +3,14 @@
        v-on:click.prevent="onClick"
        :disabled="item.disabled">
     <svg class="item__charge">
+      <path style="fill: #d0d0ee; fill-rule: evenodd; opacity: 0.2"
+        d="M18,0A18,18,0,1,1,0,18,18,18,0,0,1,18,0Zm0,6A12,12,0,1,1,6,18,12,12,0,0,1,18,6Z"/>
       <defs>
-        <path id="item__charge-delimiters-4" d="M0,0H17V17H0V0ZM19,0H36V17H19V0ZM0,19H17V36H0V19Zm19,0H36V36H19V19Z"/>
+        <path id="item__charge-delimiters"
+         d="M0,0H17V17H0V0ZM19,0H36V17H19V0ZM0,19H17V36H0V19Zm19,0H36V36H19V19Z"/>
       </defs>
       <clipPath id="item__charge-delimiters-mask">
-        <use xlink:href="#item__charge-delimiters-4" style="overflow:visible;" />
+        <use xlink:href="#item__charge-delimiters" style="overflow:visible;" />
       </clipPath>
       <circle class="item__charge-pie"
         :style="`stroke-dasharray: ${chart.dash}, ${chart.gap};`"></circle>
@@ -16,7 +19,7 @@
 </template>
 
 <script>
-const CHART_RADIUS = 18;
+const CHART_RADIUS = 15;
 const CHART_PERIMETER = Math.PI * (CHART_RADIUS * 2);
 const CHART_ANGLE_SIZE = CHART_PERIMETER / 360;
 
