068934887d701206b8ca175c105356e23b30a970
fara <buterbread@gmail.com>
2020-04-13 23:38:32 +0300

    Alpha 0.1 WIP

--- src/components/BubbleComponentRect.vue
+++ src/components/BubbleComponentRect.vue
@@ -2,15 +2,28 @@
   <div :class="['item-box', item.type ]"
        v-on:click.prevent="onClick"
        :disabled="item.disabled">
-    <svg class="item__charge" :data-maxvalue="maxValue">
+    <svg class="item__charge" :data-maxvalue="item.maxItemValue">
       <path class="item__charge-placeholder"
         d="M18,0A18,18,0,1,1,0,18,18,18,0,0,1,18,0Zm0,6A12,12,0,1,1,6,18,12,12,0,0,1,18,6Z" />
-      <circle class="item__charge-pie" :style="`stroke-dasharray: ${chart.dash}, ${chart.gap};`"></circle>
+      <circle
+        class="item__charge-pie"
+        :style="`stroke-dasharray: ${chart.dash}, ${chart.gap};`"></circle>
     </svg>
+    <div class="item__emitter-box">
+      <template v-for="(emitter, idx) in emitters">
+        <div v-if="emitter.animation" :class="['item__emitter', emitter.label]" v-bind:key="idx">
+          <div class="item__emitter-bullet">
+            <i class="item__emitter-bullet-mark"/>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex';
+
 const CHART_RADIUS = 15;
 const CHART_PERIMETER = Math.PI * (CHART_RADIUS * 2);
 const CHART_ANGLE_SIZE = CHART_PERIMETER / 360;
@@ -18,11 +31,12 @@ const CHART_ANGLE_SIZE = CHART_PERIMETER / 360;
 export default {
   props: ['item'],
   computed: {
-    maxValue() {
-      return this.$props.item.maxItemValue;
-    },
-    theValue() {
-      return this.$props.item.value;
+    emitters() {
+      const { currentLevel } = this.user;
+      const { type } = currentLevel;
+      const { item } = this.$props;
+
+      return item.emitters[type];
     },
     chart() {
       const { value, maxItemValue } = this.$props.item;
@@ -33,26 +47,36 @@ export default {
         gap: CHART_ANGLE_SIZE * (360 - angle),
       };
     },
+    ...mapState({
+      user: state => state.user,
+      itemsArray: state => state.itemsArray,
+      gameStarted: state => state.gameStarted,
+    }),
+    ...mapGetters([
+      'animationsInProgress',
+    ]),
   },
   methods: {
     onClick(event) {
-      if (!this.$store.state.gameStarted
-          || this.$props.item.disabled
-          || this.$store.getters.animationsInProgress) {
+      const { item } = this.$props;
+
+      if (!this.gameStarted
+          || item.disabled
+          || this.animationsInProgress) {
         return;
       }
 
       const { row, col } = event.currentTarget.dataset;
 
       if (event.shiftKey) {
-        this.$store.state.itemsArray[row][col].value
-          = this.$store.state.itemsArray[row][col].maxItemValue;
+        this.itemsArray[row][col].value
+          = this.itemsArray[row][col].maxItemValue;
         return;
       }
 
       if (event.altKey) {
-        this.$store.state.itemsArray[row][col].value
-          = this.$store.state.itemsArray[row][col].minItemValue;
+        this.itemsArray[row][col].value
+          = this.itemsArray[row][col].minItemValue;
         return;
       }
 
--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="playground-box">
+    <div class="playground-row" v-for="(itemRow, rowIndex) in itemsArray"
+         v-bind:key="itemRow.id">
+      <PlaygroundItem v-for="(item, colIndex) in itemRow"
+            v-bind:item="item"
+            v-bind:key="item.id"
+            v-bind:data-row="rowIndex"
+            v-bind:data-col="colIndex"></PlaygroundItem>
+    </div>
     <svg style="width: 0; height: 0">
       <defs>
         <path id="item__charge-delimiters-2"
           d="M36,19V36H-0.007V19H36ZM36,0V17H-0.007V0H36Z"/>
         <path id="item__charge-delimiters-3"
-          d="M17,36V18.577L1.046,9.366,0,10V36H17Zm2,0V18.577L34.955,9.366,36,10V36H19ZM33.955,7.634L18,16.845,2.046,7.634,0,6V0H36V6Z"/>
+          d="M17,36
+          V18.577L1.046,9.366,0,10
+          V36H17Zm2,0
+          V18.577L34.955,9.366,36,10
+          V36H19ZM33.955,7.634L18,16.845,2.046,7.634,0,6
+          V0H36V6Z"/>
         <path id="item__charge-delimiters-4"
-          d="M-8.87,18L3.86,5.27,16.591,18,3.86,30.73ZM5.272,3.858L18-8.872l12.73,12.73L18,16.588Zm0,28.284L18,19.412l12.73,12.73L18,44.873ZM19.414,18L32.145,5.27,44.875,18,32.145,30.73Z"/>
+          d="M-8.87,18
+          L3.86,5.27,16.591,18,3.86,30.73ZM5.272,3.858
+          L18-8.872l12.73,12.73
+          L18,16.588Zm0,28.284L18,19.412l12.73,12.73
+          L18,44.873ZM19.414,18L32.145,5.27,44.875,18,32.145,30.73Z"/>
         <path id="item__charge-delimiters-5"
-          d="M30.565,2.407L19.618,17.474,37.33,23.229,38,22V2H32Zm6.147,22.724L19,19.376V38H38V26ZM17,38V19.376L-0.712,25.131-2,26V38H17ZM5.435,2.407L16.382,17.474-1.33,23.229-2,22V2H4ZM28.947,1.232L18,16.3,7.053,1.232,7,0H29Z"/>
+          d="M30.565,2.407
+          L19.618,17.474,37.33,23.229,38,22
+          V2H32Zm6.147,22.724
+          L19,19.376V38H38
+          V26ZM17,38V19.376
+          L-0.712,25.131-2,26
+          V38H17ZM5.435,2.407
+          L16.382,17.474-1.33,23.229-2,22V2H4ZM28.947,1.232L18,16.3,7.053,1.232,7,0H29Z"/>
         <path id="item__charge-delimiters-6"
-          d="M19.732,17L29.547,0H36V17H19.732Zm-3.464,0H0V0H6.453Zm0,2L6.453,36H0V19H16.268ZM27.238,0L18,16,8.762,0H27.238ZM8.762,36L18,20l9.238,16H8.762Zm10.97-17H36V36H29.547Z"/>
+          d="M19.732,17
+          L29.547,0H36V17H19.732Zm-3.464,0H0V0H6.453Zm0,2
+          L6.453,36H0V19H16.268ZM27.238,0
+          L18,16,8.762,0H27.238ZM8.762,36L18,20l9.238,16H8.762Zm10.97-17H36V36H29.547Z"/>
       </defs>
       <clipPath id="item__charge-delimiters-mask-2">
         <use xlink:href="#item__charge-delimiters-2" />
@@ -29,14 +56,6 @@
         <use xlink:href="#item__charge-delimiters-6" />
       </clipPath>
     </svg>
-    <div class="playground-row" v-for="(itemRow, rowIndex) in itemsArray"
-         v-bind:key="itemRow.id">
-      <PlaygroundItem v-for="(item, colIndex) in itemRow"
-            v-bind:item="item"
-            v-bind:key="item.id"
-            v-bind:data-row="rowIndex"
-            v-bind:data-col="colIndex"></PlaygroundItem>
-    </div>
   </div>
 </template>
 
